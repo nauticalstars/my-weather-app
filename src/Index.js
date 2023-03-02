979cbb8a2115019525cc3466f4300c2dd4b086b2
nauticalstars <nauticalstars21@gmail.com>
2023-03-01 19:48:50 -0800

    Changed CSS form

--- src/Index.js
+++ src/Index.js
@@ -7,7 +7,7 @@ function formatDate(date) {
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
 
   let months = [
@@ -22,7 +22,7 @@ function formatDate(date) {
     "September",
     "October",
     "November",
-    "December"
+    "December",
   ];
   let currentYear = date.getFullYear();
   let currentDay = days[date.getDay()];
@@ -30,55 +30,58 @@ function formatDate(date) {
   let currentDate = date.getDate();
   let currentHour = date.getHours();
   let currentMin = date.getMinutes();
+  if (currentMin < 10) {
+    currentMin = `0${currentMin}`;
+  }
 
   let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear} <em><br>${currentHour}:${currentMin}</em></br>`;
 
   return formattedDate;
 }
 
-let title = document.querySelector(`#title`);
-title.innerHTML = formatDate(currentDate);
-
 function cityFormValues(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#cityInput");
   let city = `${cityInput.value}`;
-  let apiKey = "da356f86114d4368e53d5acadf7e4e28";
+  let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
   let units = "metric";
-  let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  let apiUrl2 = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl2).then(cityFormTemp);
 
   function cityFormTemp(response) {
     let h4 = document.querySelector("h4");
     let temp1 = document.querySelector("#temp1");
-    let temperature2 = Math.round(response.data.main.temp);
-    h4.innerHTML = `Today in ${response.data.name}`;
+    let temperature2 = Math.round(response.data.temperature.current);
+    h4.innerHTML = `Today in ${response.data.city}`;
     temp1.innerHTML = `${temperature2}°C`;
   }
 }
-
-let cityForm = document.querySelector("#city-form");
-cityForm.addEventListener("submit", cityFormValues);
 
 function currentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(retrievePosition);
   function retrievePosition(position) {
-    let apiKey = "da356f86114d4368e53d5acadf7e4e28";
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    let Url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
+    let Url = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}`;
+    console.log(Url);
     axios.get(Url).then(showWeather);
   }
   function showWeather(response) {
     let h4 = document.querySelector("h4");
     let temp1 = document.querySelector("#temp1");
-    let temperature = Math.round(response.data.main.temp);
-    h4.innerHTML = `Today in ${response.data.name}`;
+    let temperature = Math.round(response.data.temperature.current);
+    h4.innerHTML = `Today in ${response.data.city}`;
     temp1.innerHTML = `${temperature}°C`;
   }
 }
+let title = document.querySelector(`#title`);
+title.innerHTML = formatDate(currentDate);
+let cityForm = document.querySelector("#city-form");
+cityForm.addEventListener("submit", cityFormValues);
 let currentLoc = document.querySelector("#geoButton");
 currentLoc.addEventListener("click", currentLocation);
+
 //it should display the name of the city on the result page and the current temperature of the city.
 //Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
